46ba16ed0d83add9d5c380c072aaffb6d9c8b00f
Nikita Gorchakov <nikitagorchakov880091@gmail.com>
2020-10-15 13:09:17 +0300

    Add server configuration

--- server/app.js
+++ server/app.js
@@ -1,3 +1,27 @@
-const app = require('express')()
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config({ path: '../.env' })
 
-app.listen(5000, () => console.log('Server start ...'))
+const app = express()
+
+const PORT = process.env.PORT_FOR_DEV || 6000
+
+app.use(express.json({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
+
+const start = () => {
+  try {
+    mongoose.connect(process.env.DB_CONNECTION_FOR_DEV, {
+      useNewUrlParser: true,
+      useFindAndModify: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    }).then(() => console.log('DB connected'))
+
+    app.listen(PORT, () => {console.log(`Server start... on port ${PORT}`)})
+  } catch (e) {
+    console.log('Error on server...', e.message)
+  }
+}
+
+start()
