4df577b1e9f747204054aa5a34750d5f1bafc16f
Dmitrii <kosenkowdima@gmail.com>
2020-10-15 16:22:13 +0300

    ProfilePage added template

--- client/src/pages/ProfilePage/ProfilePage.js
+++ client/src/pages/ProfilePage/ProfilePage.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import { user } from './newUser'
+import style from './ProfilePage.module.css'
 
 const ProfilePage = () => {
   return (
-    <div>
-      <h1>ProfilePage</h1>
+    <div className={style.profileBody}>
+      <div className={style.header}>
+        <h1>ProfilePage</h1>
+        <button className={style.roundBtn}>edit</button>
+        <button className={style.roundBtn}>Log Out</button>
+      </div>
+      <div className={style.profileTop}>
+        <div className={style.containerForUserPhoto}>
+          <img className={style.userPhoto} alt={user.name} src={user.photo} />
+          <h2>
+            Raiting {user.rating}
+          </h2>
+          <h3 className={style.skills}>
+            Skills
+            <div className={style.skillsMargin}>
+              {user.skills.map(skill => <li>{skill}</li>)}
+            </div>
+          </h3>
+        </div>
+        <div className={style.userGeneralInfo}>
+          <h1 className={style.userName}>{user.name}</h1>
+            <h2>{user.status}</h2>
+            <h2><img style={{width: '1.5em'}} src='https://i.pinimg.com/originals/96/4c/f1/964cf112c8928c7c75312f7f15e6b1e0.png' /> x {user.coins}</h2>
+        </div>
+      </div>
     </div>
   )
 }
