821f101525cd11851ff0a97196ab0c084cdf6932
Nikita Gorchakov <nikitagorchakov880091@gmail.com>
2020-10-15 16:16:48 +0300

    Add configurable loaderComponent

--- client/src/pages/AuthPage/AuthPage.js
+++ client/src/pages/AuthPage/AuthPage.js
@@ -1,21 +1,9 @@
 import React from 'react'
 
-import ButtonComponent from '../../components/Button/ButtonComponent'
-import CardComponent from '../../components/Card/CardComponent'
-import InputComponent from '../../components/Input/InputComponent'
-
 const AuthPage = () => {
   return (
     <div>
       <h1>AuthPage</h1>
-      <InputComponent placeholder='Ваше Имя'/>
-      <InputComponent placeholder='Ваше Email'/>
-      <InputComponent placeholder='Ваше Пароль' type='password'/>
-      <ButtonComponent title='Name'/>
-      <CardComponent/>
-      <CardComponent/>
-      <CardComponent/>
-      <CardComponent/>
     </div>
   )
 }
