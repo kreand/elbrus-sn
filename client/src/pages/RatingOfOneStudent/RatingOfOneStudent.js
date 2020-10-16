75393627647260c5f843921a69f91595dd8d37f4
Andrey Kretsu <lutch97@gmail.com>
2020-10-16 14:26:24 +0300

    added states for points of students

--- client/src/pages/RatingOfOneStudent/RatingOfOneStudent.js
+++ client/src/pages/RatingOfOneStudent/RatingOfOneStudent.js
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ButtonComponent from '../../components/Button/ButtonComponent';
 import InputComponent from '../../components/Input/InputComponent';
 import {useHistory} from 'react-router-dom'
 
 const RatingOfOneStudent = () => {
+    const [points,setPoints] = useState(0)
 const hist = useHistory()
+    async function addPoints() {
+        setPoints(points + 1)
+    }
+    async function minusPoints() {
+        setPoints(points - 1)
+    }
     return (
         <div>
             <div>
             <h2 style={{marginLeft: 10}}>Имя Фамилия</h2>
-            <h3>Количесвто баллов</h3>
+            <h3>{points}</h3>
             </div>
-            <InputComponent justify='left' offset="0" size="small" placeholder="Сколько баллов изменить" span="2"/>
             <div style={{display: 'flex',}}>
                 <div style={{marginRight: 15, marginLeft: 10, marginBottom: 15}}>
-                    <ButtonComponent justify="left" title="Добавить" size="large"/>
+                    <ButtonComponent onClick={addPoints} justify="left" title="Добавить" size="large"/>
                 </div>
-                <ButtonComponent justify="left" title="Отнять" size="large"/>
+                <ButtonComponent onClick={minusPoints} justify="left" title="Отнять" size="large"/>
             </div>
             <textarea style={{marginLeft: 10}} placeholder="Комментарий"></textarea>
-            <span onClick={() => hist.push('/ratings')}>
-                <ButtonComponent   title="назад" size="small" justify="left"/>
-            </span>
+                <ButtonComponent onClick={() => hist.push('/ratings')} title="назад" size="small" justify="left"/>
         </div>
     );
 };
