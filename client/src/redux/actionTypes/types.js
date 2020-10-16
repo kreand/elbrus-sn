740dd0e7a647d20467a364efca989a1ebef27893
Nikita Gorchakov <nikitagorchakov880091@gmail.com>
2020-10-16 10:23:30 +0300

    Add registration and auth saga

--- client/src/redux/actionTypes/types.js
+++ client/src/redux/actionTypes/types.js
@@ -1,1 +1,9 @@
 // Profile page actions
+export const GET_USER = 'GET_USER'
+export const GET_DEFAULT_USER = 'GET_DEFAULT_USER'
+export const REGISTRATION_USER = 'REGISTRATION_USER'
+export const REGISTRATION_DEFAULT_USER = 'REGISTRATION_DEFAULT_USER'
+
+// Authorization/Registration
+export const AUTH = 'AUTH'
+export const LOGOUT = 'LOGOUT'
