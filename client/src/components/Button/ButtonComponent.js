e512be4c688f4280ea54c612b777acbddf55030e
Nikita Gorchakov <nikitagorchakov880091@gmail.com>
2020-10-16 12:50:03 +0300

    Fix onClick on Button and add login/logout fitch

--- client/src/components/Button/ButtonComponent.js
+++ client/src/components/Button/ButtonComponent.js
@@ -2,11 +2,17 @@ import React from 'react'
 import { Button, Col, Row } from 'antd'
 import style from './ButtonComponent.module.css'
 
-const ButtonComponent = ({ title, color, justify, loading ,size, type}) => {
+const ButtonComponent = ({ onClick, title, color, justify, loading, size, type }) => {
   return (
     <Row justify={justify || 'center'}>
       <Col>
-        <Button htmlType={type || 'submit'} size={size || 'middle'} loading={loading} className={style[color] || style.button} type="primary">{title}</Button>
+        <Button onClick={onClick ? () => onClick() : null}
+                htmlType={type || 'submit'}
+                size={size || 'middle'}
+                loading={loading}
+                className={style[color] || style.button}
+                type="primary">{title}
+        </Button>
       </Col>
     </Row>
   )
--- client/src/redux/reducers/authRedcuer.js
+++ client/src/redux/reducers/authRedcuer.js
@@ -1,13 +1,13 @@
 import { AUTH, LOGOUT } from '../actionTypes/types'
 
-const initState = { isAuth: false }
+const initState = false
 
 export function authReducer (state = initState, action) {
   switch (action.type) {
     case AUTH:
-      return { isAuth: true }
+      return true
     case LOGOUT:
-      return { isAuth: false }
+      return false
     default:
       return state
   }
