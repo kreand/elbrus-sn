547906cb41796786ac4f684495a3b8c86e48863b
Nikita Gorchakov <nikitagorchakov880091@gmail.com>
2020-10-18 15:23:34 +0300

    Add Layout component

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,46 +1,32 @@
+import { Layout } from 'antd';
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import LayoutComponent from './components/Layout/LayoutComponent';
+import style from './components/Layout/LayoutComponent.module.css';
 
 import AuthPage from './pages/AuthPage/AuthPage';
-import EmployersPage from './pages/EmployersPage/EmployersPage';
-import EventsPage from './pages/EventsPage/EventsPage';
-import Main from './pages/Main/Main';
-import ProfilePage from './pages/ProfilePage/ProfilePage';
-import RatingOfOneStudent from './pages/RatingOfOneStudent/RatingOfOneStudent';
-import RatingsPage from './pages/RatingsPage/RatingsPage';
 
 export const useRoutes = isAuthenticated => {
   if (isAuthenticated) {
     return (
-      <Switch>
-        <Route path="/employers" exact>
-          <EmployersPage />
-        </Route>
-        <Route path="/events" exact>
-          <EventsPage />
-        </Route>
-        <Route path="/ratings" exact>
-          <RatingsPage />
-        </Route>
-        <Route path="/profile" exact>
-          <ProfilePage />
-        </Route>
-        <Route path="/student" exact>
-          <RatingOfOneStudent />
-        </Route>
-        <Redirect to="/profile" exact/>
-        <Route path="/">
-          <Main />
-        </Route>
-      </Switch>
+      <LayoutComponent/>
     );
   }
+
+  const { Header, Footer } = Layout;
+
   return (
-    <Switch>
-      <Route path="/auth" exact>
-        <AuthPage />
-      </Route>
-      <Redirect to="/auth" />
-    </Switch>
+    <Layout className={style.body}>
+      <Header className={style.header}>
+        <h1>Elbrus Social Network</h1>
+      </Header>
+      <Switch>
+        <Route path="/auth" exact>
+          <AuthPage/>
+        </Route>
+        <Redirect to="/auth"/>
+      </Switch>
+      <Footer className={style.footer}>Footer</Footer>
+    </Layout>
   );
 };
