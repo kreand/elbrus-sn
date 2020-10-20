0399ea3940c1e6b9569c976868c8663e1cc77082
Alexander Glazyrin <glazyrin-ai@yandex.ru>
2020-10-20 09:32:36 +0300

    added sort

--- client/src/components/EmployerProfile/EmployerProfile.js
+++ client/src/components/EmployerProfile/EmployerProfile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link, useParams, useHistory} from 'react-router-dom';
 import {useSelector} from 'react-redux';
 import {Row, Col} from 'antd';
@@ -7,15 +7,46 @@ import ButtonComponent from '../Button/ButtonComponent';
 import RateComponent from '../Rate/RateComponent';
 import EmployerReviewsList from '../EmployerReviewsList/EmployerReviewsList';
 import style from './EmployerProfile.module.css';
+import {DownOutlined, UpOutlined} from '@ant-design/icons';
 
 
 const EmployerProfile = () => {
   const {id} = useParams();
   const employer = useSelector(state => state.employers.allEmployers).find(emp => emp._id === id);
+  const [emp, setEmp] = useState(employer);
   const history = useHistory();
+  const [key, setKey] = useState();
+  const [btns, setBtns] = useState({date: true, rating: true});
 
   const addReview = () => {
     history.push(`add-review-about-employer/${id}`);
+  };
+
+  useEffect(() => {
+    setKey(employer.rating);
+    setEmp(employer);
+  }, [employer]);
+
+  const sortByDate = () => {
+    if (btns.date) {
+      setEmp( {...employer, allReviews: {...employer}.allReviews.sort((a, b) => a.date - b.date)});
+    } else {
+      setEmp({...employer, allReviews: {...employer}.allReviews.sort((a, b) => b.date - a.date)});
+    }
+    setBtns(prev => {
+      return {...prev, date: !btns.date};
+    });
+  };
+
+  const sortByRating = () => {
+    if (btns.rating) {
+      setEmp( {...employer, allReviews: {...employer}.allReviews.sort((a, b) => a.rating - b.rating)});
+    } else {
+      setEmp({...employer, allReviews: {...employer}.allReviews.sort((a, b) => b.rating - a.rating)});
+    }
+    setBtns(prev => {
+      return {...prev, rating: !btns.rating};
+    });
   };
 
   return (
@@ -27,12 +58,26 @@ const EmployerProfile = () => {
         <br/>
         <h2 className={style.text}>{employer.name}</h2>
         <RateComponent
+          key={key}
           rate={employer.rating}
           justify='left'
           disabled={true}
         />
-        <ButtonComponent title='Написать отзыв' justify='left' onClick={addReview}/>
-        <EmployerReviewsList allReviews={employer.allReviews} employerId={employer._id}/>
+        <div className={style.buttonContainer}>
+          <ButtonComponent title='Написать отзыв' justify='left' onClick={addReview}/>
+          <div style={{paddingBottom: 0}}>
+            <div>
+              Сортировать:
+            </div>
+            <Link to='#' onClick={sortByDate}>
+              {btns.date ? <DownOutlined/> : <UpOutlined />}{' Дата'}
+            </Link>
+            <Link to='#' style={{marginLeft: 10}} onClick={sortByRating}>
+              {btns.rating ? <DownOutlined/> : <UpOutlined />}{' Рейтинг'}
+            </Link>
+          </div>
+        </div>
+        <EmployerReviewsList allReviews={emp.allReviews} employerId={emp._id}/>
       </Col>
     </Row>
   );
--- client/src/components/EmployerReviewsList/EmployerReviewsList.js
+++ client/src/components/EmployerReviewsList/EmployerReviewsList.js
@@ -23,11 +23,11 @@ const EmployerReviewsList = ({allReviews, employerId}) => {
             avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>}
             title={<Link to={`/student/${review.userId}`}>{review.userName}</Link>}
             description={<>
-              <RateComponent rate={review.rating} justify='left' disabled={true}/>
-              <p className={style.review}>{review.review}</p>
               <div className={style.date}>
                 {`Дата отзыва: ${new Date(review.date).getDate()}-${new Date(review.date).getMonth()}-${new Date(review.date).getFullYear()}`}
               </div>
+              <RateComponent rate={review.rating} justify='left' disabled={true} key={review._id}/>
+              <p className={style.review}>{review.review}</p>
               {user._id === review.userId ?
                 <Link
                   to='#'
