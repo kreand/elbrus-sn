a0b46fd584a92804568852ca23352c2a8cdc5dfc
Alexander Glazyrin <glazyrin-ai@yandex.ru>
2020-10-20 17:35:14 +0300

    change style of navEmployers

--- client/src/components/EmployerNav/EmployerNav.js
+++ client/src/components/EmployerNav/EmployerNav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {Menu, Col, Row} from 'antd';
+import {Col, Row} from 'antd';
 import {useDispatch, useSelector} from 'react-redux';
 import {changeActiveEmpBtn} from '../../redux/actionCreators/employerAC';
 import style from './EmployersNav.module.css';
@@ -8,28 +8,28 @@ import style from './EmployersNav.module.css';
 const EmployerNav = () => {
   const {activeTab} = useSelector(state => state.employers);
   const dispatch = useDispatch();
+
   const handleClick = e => {
-    dispatch(changeActiveEmpBtn(e.key));
+    dispatch(changeActiveEmpBtn(e.target.name));
   };
 
   return (
-    <Row justify='center' style={{marginBottom: '20px'}}>
+    <Row justify='center' style={{marginBottom: 40, marginTop: 10}}>
       <Col>
-        <Menu onClick={handleClick}
-              selectedKeys={[activeTab]}
-              mode="horizontal"
-        >
-          <Menu.Item key="all-employers" className={style.navElement}>
-            <Link to={'/employers'}>
-              Все работодатели
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="add-employer">
-            <Link to={'/add-employer'}>
-              Добавить работодателя
-            </Link>
-          </Menu.Item>
-        </Menu>
+        <Link
+          to={'/employers'}
+          name="all-employers"
+          onClick={handleClick}
+          className={activeTab === "all-employers" ? style.active : style.navElement}>
+          Все работодатели
+        </Link>
+        <Link
+          to={'/add-employer'}
+          name="add-employer"
+          onClick={handleClick}
+          className={activeTab === "add-employer" ? style.active : style.navElement}>
+          Добавить работодателя
+        </Link>
       </Col>
     </Row>
   );
