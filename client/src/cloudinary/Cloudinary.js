813d431e4d993f75e5967e2029814f35dd815b17
VanMexico <ivan@pitertsev.pw>
2020-10-21 10:22:12 +0300

    Merge master

--- client/src/cloudinary/Cloudinary.js
+++ client/src/cloudinary/Cloudinary.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
+import style from './Cloudinary.module.css';
 import { addPhotoUrl } from '../redux/actionCreators/profileAC';
 
 function Cloudinary({ folder, width }) {
